29ed424da17b0a6d4c8bba2024174089967f47f1
ThePatrickCastle <patriciosalvador@ciencias.unam.mx>
2026-02-17 13:00:12 -0600

    Adding structure

---
+++ src/main/kotlin/com/kernel/crew/sys/adogta/AdogtaApplication.kt
@@ -0,0 +1,11 @@
+package com.kernel.crew.sys.adogta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdogtaApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdogtaApplication>(*args)
+}
---
+++ src/test/kotlin/com/kernel/crew/sys/adogta/AdogtaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kernel.crew.sys.adogta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdogtaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
