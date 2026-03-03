7ef264976ff49f009a0cfd09ec5e38084bdfe91e
ThePatrickCastle <patriciosalvador@ciencias.unam.mx>
2026-03-02 23:43:57 -0600

    feat: Connecting database

--- src/main/kotlin/com/kernel/crew/sys/adogta/AdogtaApplication.kt
+++ src/main/kotlin/com/kernel/crew/sys/adogta/AdogtaApplication.kt
@@ -1,5 +1,6 @@
 package com.kernel.crew.sys.adogta
 
+import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,12 @@ import org.springframework.boot.runApplication
 class AdogtaApplication
 
 fun main(args: Array<String>) {
+
+	val dotenv = dotenv()
+
+	System.setProperty("URL_DB", dotenv["URL_DB"])
+	System.setProperty("USER_DB", dotenv["USER_DB"])
+	System.setProperty("PASSWORD_DB", dotenv["PASSWORD_DB"])
+
 	runApplication<AdogtaApplication>(*args)
 }
