678b9b1a919009b143e6310c75db74bdf9586a3b
dani <danielafg@ciencias.unam.mx>
2026-05-05 17:03:30 -0600

    fix: update AnimalRepository to use AnimalId as pk

--- src/main/kotlin/com/kernel/crew/sys/adogta/repositories/AnimalRepository.kt
+++ src/main/kotlin/com/kernel/crew/sys/adogta/repositories/AnimalRepository.kt
@@ -1,9 +1,9 @@
 package com.kernel.crew.sys.adogta.repositories
 
 import com.kernel.crew.sys.adogta.entities.AnimalEntity
+import com.kernel.crew.sys.adogta.entities.AnimalId
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AnimalRepository : JpaRepository<AnimalEntity, Long> {
-}
+interface AnimalRepository : JpaRepository<AnimalEntity, AnimalId>
