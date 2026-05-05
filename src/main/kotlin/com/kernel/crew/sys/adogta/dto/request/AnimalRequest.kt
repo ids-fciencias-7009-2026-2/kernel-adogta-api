4565eaeb44d3964d9d687ff5174c1a42e260ab39
dani <danielafg@ciencias.unam.mx>
2026-05-05 16:41:44 -0600

    feat: add override fields to AnimalRequest DTO

--- src/main/kotlin/com/kernel/crew/sys/adogta/dto/request/AnimalRequest.kt
+++ src/main/kotlin/com/kernel/crew/sys/adogta/dto/request/AnimalRequest.kt
@@ -2,19 +2,15 @@ package com.kernel.crew.sys.adogta.dto.request
 
 data class AnimalRequest(
     val usuarioId: Int,
-    
     val nombre: String,
-    val tipo: String, // perro o gato
-    val edad: Int, // En meses
+    val tipo: String,       // perro o gato
+    val edad: Int,          // en meses
     val descripcion: String,
     val codigoPostal: String,
-    
     val estadoVacunacion: String,
     val esterilizado: Boolean,
     val entrenado: Boolean,
-    
-    val idRaza: Int, 
-    
+    val idRaza: Int
     val overrideEnergia: Int? = null,
     val overrideIndependencia: Int? = null,
     val overrideSociableNiños: Int? = null,
