1c7cfead3389b9e9a14b881a95185c90d2cd3c16
dani <danielafg@ciencias.unam.mx>
2026-05-05 17:03:30 -0600

    fix: remove ñ from AnimalId fields

--- src/main/kotlin/com/kernel/crew/sys/adogta/entities/AnimalId.kt
+++ src/main/kotlin/com/kernel/crew/sys/adogta/entities/AnimalId.kt
@@ -1,11 +1,17 @@
 package com.kernel.crew.sys.adogta.entities
 
+import jakarta.persistence.Column
 import jakarta.persistence.Embeddable
 import java.io.Serializable
 
 @Embeddable
 data class AnimalId(
-    val id_animal: Int = 0,
-    val id_publicacion: Int = 0,
-    val id_usuario: Int = 0
+    @Column(name = "id_animal")
+    val idAnimal: Int = 0,
+
+    @Column(name = "id_publicacion")
+    val idPublicacion: Int = 0,
+
+    @Column(name = "id_usuario")
+    val idUsuario: Int = 0
 ) : Serializable
